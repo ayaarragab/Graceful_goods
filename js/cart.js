459d0fcf8213aaf264be9292adb9fde3377561fc
ayaarragab <ayarsk4343@gmail.com>
2023-11-21 14:40:51 +0200

    Edits on cart.js

--- js/cart.js
+++ js/cart.js
@@ -14,50 +14,50 @@ if (checkLoginStatus()) {
   cart.innerHTML += `<h2 class="text-center">Please login to add products to the cart.</h2>`;
 }
 
-(async () => {
-    function getCartFromLocalStorage() {
-      const cart = localStorage.getItem('cart');
-      return cart ? JSON.parse(cart) : [];
-    }
-  
-    const productIds = getCartFromLocalStorage();
-  
-    async function fetchProductDetails(productId) {
-      const response = await fetch(`https://fakestoreapi.com/products/${productId}`);
-      if (response.ok) {
-        const productData = await response.json();
-        return productData;
-      } else {
-        console.error('Error fetching product details:', response.status);
-        return null;
-      }
-    }
-  
-    const productsPromises = productIds.map(fetchProductDetails);
-    const products = await Promise.all(productsPromises);
-  
-    function displayProducts(products) {
-        const productListContainer = document.getElementById('cartProducts');
-      
-        products.forEach(product => {
-          const productElement = createProductElement(product);
-          productListContainer.innerHTML += productElement;
-        });
-      }
+async function fetchAndDisplayProducts() {
+  function getCartFromLocalStorage() {
+    const cart = localStorage.getItem('cart');
+    return cart ? JSON.parse(cart) : [];
+  }
 
-  
-    function createProductElement(product) {
-      const productElement = `<div class="product m-4">
-                <div class="product-img">
-                    <img src="${product.image}" alt="">
-                </div>
-                <h3>${product.title}</h3>
-            <div class="price">
-                <span>$${product.price}</span>
-            </div>`;
-      return productElement;
+  const productIds = getCartFromLocalStorage();
+
+  async function fetchProductDetails(productId) {
+    const response = await fetch(`https://fakestoreapi.com/products/${productId}`);
+    if (response.ok) {
+      const productData = await response.json();
+      return productData;
+    } else {
+      console.error('Error fetching product details:', response.status);
+      return null;
     }
-  
-    displayProducts(products);
-  })();
-  
+  }
+
+  const productsPromises = productIds.map(fetchProductDetails);
+  const products = await Promise.all(productsPromises);
+
+  function displayProducts(products) {
+    const productListContainer = document.getElementById('cartProducts');
+
+    for (let i = 0; i < products.length; i++) {
+      const productElement = createProductElement(products[i]);
+      productListContainer.innerHTML += productElement;
+    }
+  }
+
+  function createProductElement(product) {
+    const productElement = `<div class="product m-4">
+            <div class="product-img">
+                <img src="${product.image}" alt="">
+            </div>
+            <h3>${product.title}</h3>
+        <div class="price">
+            <span>$${product.price}</span>
+        </div>`;
+    return productElement;
+  }
+
+  displayProducts(products);
+}
+
+document.addEventListener('DOMContentLoaded', fetchAndDisplayProducts);
